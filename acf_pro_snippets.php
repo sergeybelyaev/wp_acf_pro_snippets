3b1f642ac25a8555c934d61875a9b375c1465d7f
Sergey Belyaev <sergey.belyaev@w3.co>
2017-03-20 13:39:48 +0200

    Relationship snippet added

--- acf_pro_snippets.php
+++ acf_pro_snippets.php
@@ -40,3 +40,12 @@
 		<?php endwhile; ?>
 	</ul>
 <?php endif; ?>
+
+<?php // ACF Relationship ?>
+<?php if ( $posts_objects = get_field( 'posts_objects' ) ) : ?>
+	<ul>
+		<?php foreach ( $posts_objects as $post ) : setup_postdata( $post ); ?>
+			<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+		<?php endforeach; wp_reset_postdata(); ?>
+	</ul>
+<?php endif; ?>
